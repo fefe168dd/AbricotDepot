837fa3eff93e067e14b1103f65186699229949c9
Dirand <felicien.dirand1@gmail.com>
2025-10-20 17:52:11 +0200

    Remove user_id from reservations table and ensure proper foreign key constraints in reservation_user table

--- sql/ReservationSchema.sql
+++ sql/ReservationSchema.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS "reservations" CASCADE;
 CREATE TABLE "public"."reservations" (
     "id" UUID PRIMARY KEY,
-    "user_id" UUID NOT NULL,
     "outil_id" UUID NOT NULL,
     "quantity" INT NOT NULL,
     "start_date" TIMESTAMP NOT NULL,
@@ -9,4 +8,12 @@ CREATE TABLE "public"."reservations" (
     "status" VARCHAR(50) NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY("user_id") REFERENCES public.users(id) ON DELETE CASCADE,
     CONSTRAINT fk_outil FOREIGN KEY("outil_id") REFERENCES public.outil(id) ON DELETE CASCADE
+);
+DROP TABLE IF EXISTS "reservation_user" CASCADE;
+CREATE TABLE "public"."reservation_user" (
+    "reservation_id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    CONSTRAINT pk_reservation_user PRIMARY KEY ("reservation_id", "user_id"),
+    CONSTRAINT fk_reservation FOREIGN KEY("reservation_id") REFERENCES public.reservations(id) ON DELETE CASCADE,
+    CONSTRAINT fk_user FOREIGN KEY("user_id") REFERENCES public.users(id) ON DELETE CASCADE
 );
