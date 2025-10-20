5fa378bc6f674d5f68d9764f30e67d9118bc8f4c
Dirand <felicien.dirand1@gmail.com>
2025-10-20 23:58:41 +0200

    Mise à jour du type de la colonne status dans la table reservations de VARCHAR à INT et ajout des données de réservation

--- sql/ReservationSchema.sql
+++ sql/ReservationSchema.sql
@@ -5,7 +5,7 @@ CREATE TABLE "public"."reservations" (
     "quantity" INT NOT NULL,
     "start_date" TIMESTAMP NOT NULL,
     "end_date" TIMESTAMP NOT NULL,
-    "status" VARCHAR(50) NOT NULL,
+    "status" INT NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY("user_id") REFERENCES public.users(id) ON DELETE CASCADE,
     CONSTRAINT fk_outil FOREIGN KEY("outil_id") REFERENCES public.outil(id) ON DELETE CASCADE
 );
