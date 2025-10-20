7b3a23b67b591be23dfa5960b11f48b7618bd5a5
Dirand <felicien.dirand1@gmail.com>
2025-10-20 17:43:51 +0200

    Add GetOutilByCategorie class for retrieving tools by category

---
+++ app/src/api/actions/GetOutilByCategorie.php
@@ -0,0 +1,31 @@
+<?php 
+namespace App\infrastructure\repository;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\core\application\ports\spi\repositoryInterface\OutilRepository;
+
+class GetOutilByCategorie 
+{
+    private OutilRepository $outilRepository;
+
+    public function __construct(OutilRepository $outilRepository)
+    {
+        $this->outilRepository = $outilRepository;
+    }
+
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        $categorieName = $args['categorieName'];
+        $outils = $this->outilRepository->OutilParCategorie($categorieName);
+
+        if (empty($outils)) {
+            $response->getBody()->write(json_encode(['error' => 'No outils found for this category']));
+            return $response->withStatus(404)->withHeader('Content-Type', 'application/json');
+        }
+
+        $outilsData = array_map(fn($outil) => new \App\core\application\ports\api\dto\OutilDTO($outil), $outils);
+        $response->getBody()->write(json_encode($outilsData));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
