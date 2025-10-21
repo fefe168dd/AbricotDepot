dcbe4c600f428542d56f4ce1f922103d0d042c95
Dirand <felicien.dirand1@gmail.com>
2025-10-21 11:23:51 +0200

    Refonte du schéma et des données pour les outils et réservations
    Mise à jour de OutilSchema.sql : suppression des tables existantes et redéfinition des tables "outil", "categorie" et "outil_categorie" avec de nouvelles clés primaires et étrangères.
    Modification de Reservation.data.sql : insertion de nouvelles réservations avec des UUID et quantités mis à jour.
    Révision de ReservationSchema.sql : simplification de la structure de la table des réservations et mise en commentaire de la table reservation_user.
    Amélioration de Stock.data.sql : nouveaux stocks et ajustement des quantités.
    Simplification de StockSchema.sql : suppression de contraintes étrangères inutiles tout en conservant les clés primaires.

--- sql/ReservationSchema.sql
+++ sql/ReservationSchema.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS "reservations" CASCADE;
+
 CREATE TABLE "public"."reservations" (
     "id" UUID PRIMARY KEY,
     "outil_id" UUID NOT NULL,
     "quantity" INT NOT NULL,
     "start_date" TIMESTAMP NOT NULL,
     "end_date" TIMESTAMP NOT NULL,
-    "status" INT NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY("user_id") REFERENCES public.users(id) ON DELETE CASCADE,
-    CONSTRAINT fk_outil FOREIGN KEY("outil_id") REFERENCES public.outil(id) ON DELETE CASCADE
+    "status" INT NOT NULL
 );
+/*
 DROP TABLE IF EXISTS "reservation_user" CASCADE;
 CREATE TABLE "public"."reservation_user" (
     "reservation_id" UUID NOT NULL,
@@ -17,3 +17,4 @@ CREATE TABLE "public"."reservation_user" (
     CONSTRAINT fk_reservation FOREIGN KEY("reservation_id") REFERENCES public.reservations(id) ON DELETE CASCADE,
     CONSTRAINT fk_user FOREIGN KEY("user_id") REFERENCES public.users(id) ON DELETE CASCADE
 );
+*/
