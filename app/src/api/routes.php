395b39a216266d8c71ef587ad5a0bb10111b37c6
Flyguss <crafteur55100-verdun@hotmail.com>
2025-10-21 09:56:40 +0200

    Route pour l'api

--- app/src/api/routes.php
+++ app/src/api/routes.php
@@ -5,8 +5,15 @@ declare(strict_types=1);
 return function(\Slim\App $app):\Slim\App {
 
 
-
-
+    $app->post('/reservations', \abricotdepot\api\actions\AddReservationAction::class);
+    $app->get('/reservations', \abricotdepot\api\actions\GetReservationAction::class);
+    $app->get('/reservation/{id}' , \abricotdepot\api\actions\GetReservationByIDAction::class) ;
+    $app->get('/outils', \abricotdepot\api\actions\GetOutilbyidAction::class);
+    $app->get('/outils/{id}', \abricotdepot\api\actions\GetOutilbyidAction::class);
+    $app->get('/categories/{categorieName}/outils' , \abricotdepot\infrastructure\repository\GetOutilByCategorie::class);
+    $app->get('/stocks', \abricotdepot\api\actions\GetStockAction::class);
+    $app->get('/stocks/{id}', \abricotdepot\api\actions\GetStockByIdAction::class);
+    $app->get('/outils/{id}/stocks' , \abricotdepot\api\actions\GetStockByIdOutilAction::class);
   
 
     return $app;
