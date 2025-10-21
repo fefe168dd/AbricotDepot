df27cac775c04e07b5e50d8e9a99beb2294278f5
Ryad <kaderdu10120@gmail.com>
2025-10-21 11:19:29 +0200

    inscription

---
+++ app/src/web/actions/InscriptionAction.php
@@ -0,0 +1,25 @@
+<?php
+namespace abricotdepot\web\actions ;
+
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class InscriptionAction
+{
+    public function __invoke(Request $request, Response $response): Response
+    {
+        $file = __DIR__ . '/../../../public/html/inscription.html';
+
+
+        if (!file_exists($file)) {
+            $response->getBody()->write('Erreur : fichier HTML introuvable.');
+            return $response->withStatus(500);
+        }
+
+        $html = file_get_contents($file);
+
+        $response->getBody()->write($html);
+        return $response->withHeader('Content-Type', 'text/html');
+    }
+}
