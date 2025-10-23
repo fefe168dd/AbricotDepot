cf9a1b12528916a9e7b3fa7d29e5fd2cb92f304f
Flyguss <crafteur55100-verdun@hotmail.com>
2025-10-23 10:18:38 +0200

    Front end de la connexion et l'inscription avec le scss ainsi que pour ces même page la mise en place du SPA

--- app/src/web/actions/ConnexionAction.php
+++ app/src/web/actions/ConnexionAction.php
@@ -8,7 +8,7 @@ class ConnexionAction
 {
     public function __invoke(Request $request, Response $response): Response
     {
-        $file = __DIR__ . '/../../../public/html/connexion.html';
+        $file = __DIR__ . '/../../../public/html/accueil.html';
 
 
         if (!file_exists($file)) {
@@ -17,6 +17,21 @@ class ConnexionAction
         }
 
         $html = file_get_contents($file);
+
+        $connexionHTML = '<div>
+        <h1>Connectez-vous</h1>
+    </div>
+    <div class="formulaire">
+    <form class="formconnexion" method="post" action="/connexion">
+        <div class="identifiant">Adresse mail : <input type="email" name="identifiant" required></div>
+        <div class="password">Mot de passe : <input type="password" name="passw ord" required></div>
+        <div class="submit"><button type="submit">S\'inscrire</button></div>
+        <div class="inscription"><a href="/inscription">Pas de compte ? Inscrivez-vous !</a></div>
+    </form>
+    </div>' ;
+
+
+        $html = str_replace('{{Liste Outil}}', $connexionHTML, $html);
 
         $response->getBody()->write($html);
         return $response->withHeader('Content-Type', 'text/html');
