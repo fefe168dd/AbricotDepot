9d1768278a6356695a977482fffd6bf11856b8af
U.N.Owen <lclerc561@gmail.com>
2025-10-23 18:08:22 +0200

    panier pdo, repository et shema bbd modifié

--- sql/PanierSchema.sql
+++ sql/PanierSchema.sql
@@ -2,17 +2,9 @@ DROP TABLE IF EXISTS "panier";
 CREATE TABLE "public"."panier" (
     "id" UUID NOT NULL,
     "user_id" UUID NOT NULL,
-    CONSTRAINT "panier_pk" PRIMARY KEY ("id")
-);
-
-DROP TABLE IF EXISTS "panier_item";
-CREATE TABLE "public"."panier_item" (
-    "id" UUID NOT NULL,
-    "panier_id" UUID NOT NULL,
     "outil_id" UUID NOT NULL,
     "quantity" INT NOT NULL,
     "datedebut" TIMESTAMP NOT NULL,
     "datefin" TIMESTAMP NOT NULL,
-    CONSTRAINT "panier_item_pk" PRIMARY KEY ("id"),
-    CONSTRAINT "panier_item_fk" FOREIGN KEY ("panier_id") REFERENCES "panier"("id")
+    CONSTRAINT "panier_item_pk" PRIMARY KEY ("id")
 );
